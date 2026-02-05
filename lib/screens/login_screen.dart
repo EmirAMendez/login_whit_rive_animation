0b02e8e45955894340cedde30711753da50df7fe
EmirAMendez <emirmendez2114@gmail.com>
2026-02-05 11:01:08 -0600

    feat(rive):animation added

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      //Evita que se quite el espacio del nudge
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(child: RiveAnimation.asset('animated_login_bear.riv')),
+          ]
+        ),
+      ),
+    );
+  }
+}
